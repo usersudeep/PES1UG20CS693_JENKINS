c1d14f213a518df8f2f0275242ee26aeca5365e6
usersudeep <119527118+usersudeep@users.noreply.github.com>
2023-03-05 22:47:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,2 @@
-//File: hello.cpp
-#include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
-  return 0;
-}
+hello_exec: hello.cpp
+	g++ hello.cpp -o hello_exec
